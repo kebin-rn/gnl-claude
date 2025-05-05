7a168b9c3c353bb6f01b8947c5cf28a9d67e8400
けびん <69067099+kebin-rn@users.noreply.github.com>
2025-05-05 06:28:07 +0000

    fix by Claude

--- get_next_line.c
+++ get_next_line.c
@@ -36,12 +36,19 @@ static char	*read_to_stash(int fd, char *stash)
 	return (stash);
 }
 
-static char	*create_line(char *stash, int len)
+static char	*extract_line(char *stash)
 {
-	char	*line;
 	int		i;
+	char	*line;
 
-	line = malloc(sizeof(char) * (len + 1));
+	i = 0;
+	if (!stash || !stash[i])
+		return (NULL);
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (stash[i] == '\n')
+		i++;
+	line = malloc(sizeof(char) * (i + 1));
 	if (!line)
 		return (NULL);
 	i = 0;
@@ -59,46 +66,11 @@ static char	*create_line(char *stash, int len)
 	return (line);
 }
 
-static char	*extract_line(char *stash)
-{
-	int		i;
-
-	i = 0;
-	if (!stash || !stash[i])
-		return (NULL);
-	while (stash[i] && stash[i] != '\n')
-		i++;
-	if (stash[i] == '\n')
-		i++;
-	return (create_line(stash, i));
-}
-
-static char	*create_new_stash(char *stash, int i)
-{
-	char	*new_stash;
-	int		j;
-
-	new_stash = malloc(sizeof(char) * (ft_strlen(stash) - i + 1));
-	if (!new_stash)
-	{
-		free(stash);
-		return (NULL);
-	}
-	j = 0;
-	while (stash[i])
-	{
-		new_stash[j] = stash[i];
-		j++;
-		i++;
-	}
-	new_stash[j] = '\0';
-	free(stash);
-	return (new_stash);
-}
-
 static char	*update_stash(char *stash)
 {
-	int	i;
+	int		i;
+	int		j;
+	char	*new_stash;
 
 	i = 0;
 	if (!stash)
@@ -111,7 +83,18 @@ static char	*update_stash(char *stash)
 		return (NULL);
 	}
 	i++;
-	return (create_new_stash(stash, i));
+	new_stash = malloc(sizeof(char) * (ft_strlen(stash) - i + 1));
+	if (!new_stash)
+	{
+		free(stash);
+		return (NULL);
+	}
+	j = 0;
+	while (stash[i])
+		new_stash[j++] = stash[i++];
+	new_stash[j] = '\0';
+	free(stash);
+	return (new_stash);
 }
 
 char	*get_next_line(int fd)
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -52,17 +52,21 @@ char	*ft_strjoin(char *s1, char *s2)
 			return (NULL);
 		s1[0] = '\0';
 	}
+	if (!s2)
+		return (s1);
 	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
-		return (free(s1), NULL);
+	{
+		free(s1);
+		return (NULL);
+	}
 	i = 0;
 	while (s1[i])
-	{
-		str[i] = s1[i];
-		i++;
-	}
+		str[i] = s1[i++];
 	j = 0;
 	while (s2[j])
 		str[i++] = s2[j++];
-	return (str[i] = '\0', free(s1), str);
+	str[i] = '\0';
+	free(s1);
+	return (str);
 }
